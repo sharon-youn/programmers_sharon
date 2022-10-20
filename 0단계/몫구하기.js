6bbd12f4735df18ea7fed99b71beb5b32fcf9e98
sharon-youn <jos9187@gmail.com>
2022-10-20 23:15:57 +0900

    1단계 정수판별

---
+++ 0단계/몫구하기.js
@@ -0,0 +1,10 @@
+// 나누기 / 연산자 쓰고 나머지 버리기
+function solution(num1, num2) {
+    return Math.floor(num1/ num2)
+}
+
+// parseInt로도 몫만 구할 수 있다! 
+// 나누기에서 정수로 구할 경우 parseInt로 정수형으로 변환 됨
+function solution2(num1, num2) {
+    return parseInt(num1/ num2)
+}
